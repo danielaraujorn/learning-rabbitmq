034bc7e0aa13219c08810468ba5093d07d6c2d5b
Daniel Araújo <daniel.araujorn01@gmail.com>
2019-02-27 17:59:04 -0300

    tutorial dois

--- receive.js
+++ receive.js
@@ -2,16 +2,23 @@ var amqp = require("amqplib/callback_api");
 
 amqp.connect("amqp://localhost", (err, conn) => {
   conn.createChannel((err, ch) => {
-    var q = "hello";
+    var q = "task_queue";
 
-    ch.assertQueue(q, { durable: false });
+    ch.assertQueue(q, { durable: true });
+    ch.prefetch(1);
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(
       q,
       msg => {
+        var secs = msg.content.toString().split(".").length - 1;
+
         console.log(" [x] Received %s", msg.content.toString());
+        setTimeout(() => {
+          console.log(" [x] Done");
+          ch.ack(msg);
+        }, secs * 5000);
       },
-      { noAck: true }
+      { noAck: false }
     );
   });
 });
--- send.js
+++ send.js
@@ -1,12 +1,16 @@
 var amqp = require("amqplib/callback_api");
 
-amqp.connect("amqp://localhost", function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    var q = "hello";
+amqp.connect("amqp://localhost", (err, conn) => {
+  conn.createChannel((err, ch) => {
+    var q = "task_queue";
+    var msg = process.argv.slice(2).join(" ") || "Hello World!";
 
-    ch.assertQueue(q, { durable: false });
-    // Note: on Node 6 Buffer.from(msg) should be used
-    ch.sendToQueue(q, new Buffer.from("Hello World!"));
-    console.log(" [x] Sent 'Hello World!'");
+    ch.assertQueue(q, { durable: true });
+    ch.sendToQueue(q, new Buffer.from(msg), { persistent: true });
+    console.log(" [x] Sent '%s'", msg);
   });
+  setTimeout(() => {
+    conn.close();
+    process.exit(0);
+  }, 500);
 });
